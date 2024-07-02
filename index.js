7ec34ce0316fd4cf894eabdce54684b22c9c4f03
JNievas87 <jlnievas87@gmail.com>
2024-07-02 09:54:01 -0300

    CRUM Generos

--- index.js
+++ index.js
@@ -149,6 +149,78 @@ app.delete('/directores/delete/:Id', async (req, res)=>{
     }
 });
 
+//CRUM GENEROS
+//Trae todos los generos
+app.get('/generos/getall', async (req, res) => {
+    const sql = 'SELECT * FROM generos ORDER BY generos.id asc';
+    try {
+        const connection = await pool.getConnection()
+        const [listTodosGeneros] = await connection.query(sql);
+        connection.release();
+        res.json(listTodosGeneros);
+
+    } catch (error) {
+        res.send(500).send('Error al cargar todos los generos');
+    }
+});
+
+//Trae un genero por Id
+app.get('/generos/get/:id', async (req, res)=>{
+    const id= req.params.id;
+    const sql= 'SELECT * FROM generos ORDER BY generos.id asc';
+    try {
+        const connection = await pool.getConnection()
+        const [generosPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(generosPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al buscar el genero');
+    }
+});
+
+//Genera un genero nuevo
+app.post('/generos/insert', async (req, res) =>{
+    const genero = req.body;
+    const sql = 'INSERT INTO generos SET ?';
+    try {
+        const connection = await pool.getConnection()
+        const [addGenero] = await connection.query(sql, [genero]);
+        connection.release();
+        res.json(addGenero[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo agregar al genero');
+    }
+});
+
+//Actualizar genero
+app.put('/generos/update/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const genero = req.body;
+    const sql = 'UPDATE generos SET ? WHERE generos.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [actualizarGenero] = await connection.query(sql, [genero, id]);
+        connection.release();
+        res.json(actualizarGenero[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo actualizar al genero');
+    }
+});
+
+//Borrar director
+app.delete('/generos/delete/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const sql = 'DELETE FROM generos WHERE generos.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [deletPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(deletPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al eliminar el genero');
+    }
+});
+
 //CRUM USUARIOS
 //Traer todos los usuarios
 app.get('/usuarios/getall', async (req, res) => {
