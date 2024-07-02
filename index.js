0a4081bb0bf677db1dc831d788928367933895f6
JNievas87 <jlnievas87@gmail.com>
2024-07-02 09:25:59 -0300

    CRUD pelis - users

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import pool from './config/db.js';
 import 'dotenv/config';
 
 const app = express();
-
 app.use(express.json());
 
 //Trae todas las pelis
-app.get('/peliculas', async (req, res) => {
+app.get('/peliculas/getall', async (req, res) => {
     const sql = 'select peliculas.titulo, peliculas.fecha_lanzamiento, peliculas.descripcion, generos.nombre AS genero, directores.apellido AS apellido_direc, directores.nombre AS nombre_direct FROM peliculas JOIN generos ON peliculas.genero_id = generos.id JOIN directores ON peliculas.director_id = directores.id ORDER by peliculas.titulo asc';
     try {
         const connection = await pool.getConnection()
@@ -16,12 +15,12 @@ app.get('/peliculas', async (req, res) => {
         res.json(listTodasPelis);
 
     } catch (error) {
-        res.send(500).send('Internal server error');
+        res.send(500).send('Error al cargar todas las peliculas');
     }
 });
 
 //Trae una peli por Id
-app.get('/peliculas/:id', async (req, res)=>{
+app.get('/peliculas/get/:id', async (req, res)=>{
     const id= req.params.id;
     const sql= 'select peliculas.titulo, peliculas.fecha_lanzamiento, peliculas.descripcion, generos.nombre AS genero, directores.apellido AS apellido_direc, directores.nombre AS nombre_direct FROM peliculas JOIN generos ON peliculas.genero_id = generos.id JOIN directores ON peliculas.director_id = directores.id WHERE peliculas.id =?';
     try {
@@ -30,28 +29,55 @@ app.get('/peliculas/:id', async (req, res)=>{
         connection.release();
         res.json(PeliPorId[0]);
     } catch (error) {
-        res.send(500).send('Internal server error');
+        res.send(500).send('Error al buscar la pelicula');
     }
 });
 
-
 //Genera una pelicula nueva
-app.post('/peliculas', (req, res) =>{
-
+app.post('/peliculas/insert', async (req, res) =>{
+    const pelicula = req.body;
+    const sql = 'INSERT INTO peliculas SET ?';
+    try {
+        const connection = await pool.getConnection()
+        const [addPeli] = await connection.query(sql, [pelicula]);
+        connection.release();
+        res.json(addPeli[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo agregar la pelicula');
+    }
 });
 
 //Actualizar pelicula
-app.put('/peliculas/:Id', (req, res)=>{
-
+app.put('/peliculas/update/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const pelicula = req.body;
+    const sql = 'UPDATE peliculas SET ? WHERE peliculas.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [actualizarPeli] = await connection.query(sql, [pelicula, id]);
+        connection.release();
+        res.json(actualizarPeli[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo actualizar la pelicula');
+    }
 });
 
 //Borrar pelicula
-app.delete('/peliculas/:Id', (req, res)=>{
-    
+app.delete('/peliculas/delete/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const sql = 'DELETE FROM peliculas WHERE peliculas.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [deletPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(deletPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al eliminar la pelicula');
+    }
 });
 
 //Traer todos los usuarios
-app.get('/usuarios', async (req, res) => {
+app.get('/usuarios/getall', async (req, res) => {
     const sql = 'select * FROM usuarios ORDER BY usuarios.id asc';
     try {
         const connection = await pool.getConnection()
@@ -60,12 +86,12 @@ app.get('/usuarios', async (req, res) => {
         res.json(listTodosUsuarios);
 
     } catch (error) {
-        res.send(500).send('Internal server error');
+        res.send(500).send('Error al querer ver todos los Usuarios');
     }
 });
 
 //Trae una usuario por Id
-app.get('/ususarios/:id', async (req, res)=>{
+app.get('/usuarios/get/:id', async (req, res)=>{
     const id= req.params.id;
     const sql= 'select * FROM usuarios WHERE usuarios.id =?';
     try {
@@ -74,7 +100,50 @@ app.get('/ususarios/:id', async (req, res)=>{
         connection.release();
         res.json(UsuarioPorId[0]);
     } catch (error) {
-        res.send(500).send('Internal server error');
+        res.send(500).send('Error al querer buscar el usuario');
+    }
+});
+
+//Genera un usuario nuevo
+app.post('/usuarios/insert', async (req, res) =>{
+    const usuario = req.body;
+    const sql = 'INSERT INTO usuario SET ?';
+    try {
+        const connection = await pool.getConnection()
+        const [addUser] = await connection.query(sql, [usuario]);
+        connection.release();
+        res.json(addUser[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo agregar el usuario');
+    }
+});
+
+//Actualizar usuario
+app.put('/usuarios/update/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const usuario = req.body;
+    const sql = 'UPDATE usuarios SET ? WHERE usuarios.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [actualizarUser] = await connection.query(sql, [usuario, id]);
+        connection.release();
+        res.json(actualizarUser[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo actualizar el usuario');
+    }
+});
+
+//Borrar usuario
+app.delete('/usuarios/delete/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const sql = 'DELETE FROM usuarios WHERE usuarios.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [deletPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(deletPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al eliminar el usuario');
     }
 });
 
