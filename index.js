44d1c67b692601ebf181339bab57d75cf2efe91c
JNievas87 <jlnievas87@gmail.com>
2024-07-02 09:44:49 -0300

    CRUM directores

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 const app = express();
 app.use(express.json());
 
+//CRUM PELICULAS
 //Trae todas las pelis
 app.get('/peliculas/getall', async (req, res) => {
     const sql = 'select peliculas.titulo, peliculas.fecha_lanzamiento, peliculas.descripcion, generos.nombre AS genero, directores.apellido AS apellido_direc, directores.nombre AS nombre_direct FROM peliculas JOIN generos ON peliculas.genero_id = generos.id JOIN directores ON peliculas.director_id = directores.id ORDER by peliculas.titulo asc';
@@ -76,6 +77,79 @@ app.delete('/peliculas/delete/:Id', async (req, res)=>{
     }
 });
 
+//CRUM DIRECTORES
+//Trae todos los directores
+app.get('/directores/getall', async (req, res) => {
+    const sql = 'SELECT * FROM directores ORDER BY directores.id asc';
+    try {
+        const connection = await pool.getConnection()
+        const [listTodosDirectores] = await connection.query(sql);
+        connection.release();
+        res.json(listTodosDirectores);
+
+    } catch (error) {
+        res.send(500).send('Error al cargar todos los directores');
+    }
+});
+
+//Trae un director por Id
+app.get('/directores/get/:id', async (req, res)=>{
+    const id= req.params.id;
+    const sql= 'SELECT * FROM directores ORDER BY directores.id asc';
+    try {
+        const connection = await pool.getConnection()
+        const [diectoresPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(diectoresPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al buscar el director');
+    }
+});
+
+//Genera un director nuevo
+app.post('/directores/insert', async (req, res) =>{
+    const director = req.body;
+    const sql = 'INSERT INTO directores SET ?';
+    try {
+        const connection = await pool.getConnection()
+        const [addDirec] = await connection.query(sql, [director]);
+        connection.release();
+        res.json(addDirec[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo agregar al Director');
+    }
+});
+
+//Actualizar director
+app.put('/directores/update/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const director = req.body;
+    const sql = 'UPDATE directores SET ? WHERE directores.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [actualizarDirector] = await connection.query(sql, [director, id]);
+        connection.release();
+        res.json(actualizarDirector[0]);
+    } catch (error) {
+        res.send(500).send('Error - No se pudo actualizar al director');
+    }
+});
+
+//Borrar director
+app.delete('/directores/delete/:Id', async (req, res)=>{
+    const id = req.params.Id;
+    const sql = 'DELETE FROM directores WHERE directores.id = ?';
+    try {
+        const connection = await pool.getConnection()
+        const [deletPorId] = await connection.query(sql, [id]);
+        connection.release();
+        res.json(deletPorId[0]);
+    } catch (error) {
+        res.send(500).send('Error al eliminar al director');
+    }
+});
+
+//CRUM USUARIOS
 //Traer todos los usuarios
 app.get('/usuarios/getall', async (req, res) => {
     const sql = 'select * FROM usuarios ORDER BY usuarios.id asc';
